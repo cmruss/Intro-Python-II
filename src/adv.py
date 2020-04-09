47645b2f2d1d957f49447b16fcb1416454d2d792
cmruss <cmrussell08@gmail.com>
2020-04-09 02:06:35 -0400

    created room and player class

--- src/adv.py
+++ src/adv.py
@@ -3,8 +3,7 @@ from room import Room
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside':  Room("Outside Cave Entrance",  "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
@@ -38,6 +37,9 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+from player import Player
+
+player = Player(room['outside'])
 
 # Write a loop that:
 #
@@ -49,3 +51,4 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
